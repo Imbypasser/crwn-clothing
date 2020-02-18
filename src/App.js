e8599234207998230aa3c32d107d9366959d5fb2
Imbypasser <34995139+Imbypasser@users.noreply.github.com>
2020-02-18 10:51:27 +0800

    Updating our app with a shop page, collection preview component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
-import './App.css';
+import "./App.css";
 
-function App () {
-	return (
-		<div>
-			{/* The moment switch reach it's the url that match. Nothing else is render after that */}
-			<Switch>
-				<Route exact path='/' component={HomePage} />
-				<Route path='/shop' component={ShopPage} />
-			</Switch>
-		</div>
-	);
+function App() {
+  return (
+    <div>
+      {/* The moment switch reach it's the url that match. Nothing else is render after that */}
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
